61ce3fb46e2012f61e728b67a1ca8da7ccbeb45e
Alexander Lambert <rubick12545@gmail.com>
2025-11-22 19:30:39 +0200

    Day 6 fix

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -4,9 +4,16 @@ import { OrderController } from './order.controller';
 import { PrismaService } from 'src/prisma.service';
 import { UserService } from 'src/user/user.service';
 import { BucketService } from 'src/bucket/bucket.service';
+import { RedisService } from 'src/redis/redis.service';
 
 @Module({
-  providers: [OrderService, PrismaService, UserService, BucketService],
+  providers: [
+    OrderService,
+    PrismaService,
+    UserService,
+    BucketService,
+    RedisService,
+  ],
   controllers: [OrderController],
 })
 export class OrderModule {}
